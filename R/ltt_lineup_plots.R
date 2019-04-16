719b1b8fd356b5b0dc5fa03f693c767157756957
Karsten Maurer <maurerkt@miamioh.edu>
2019-04-16 14:37:54 -0400

    add lineup plot function

---
+++ R/ltt_lineup_plots.R
@@ -0,0 +1,26 @@
+#' Function to generate tea-tasting lineup
+#'
+#' @param lineup_dat dataframe structured from \code{\link{make_lineup_dat}}
+#'
+#' @return ggplot
+#' @export
+#'
+#' @examples
+#' dat=data.frame(x=rnorm(50))
+#' dat$y=x+rnorm(50)
+#' lineup_dat = make_lineup_dat(M=8, dat, xname="x", yname="y")
+#' tt_lineup_plot(lineup_dat)
+tt_lineup_plot = function(lineup_dat){
+  lineup_dat$facet_lab = paste("Plot",lineup_dat$order)
+  lineup_dat$facet_lab = factor(lineup_dat$facet_lab,
+                                levels=paste("Plot",min(lineup_dat$order):max(lineup_dat$order)) )
+  ggplot2::ggplot()+
+    ggplot2::geom_point(aes(x=x,y=permy), data=lineup_dat) +
+    ggplot2::facet_wrap(~facet_lab)+
+    ggplot2::labs(x="",y="")+
+    ggplot2::theme_bw()+
+    ggplot2::theme(panel.spacing = unit(.75, "cm"),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title = element_blank())
+}
